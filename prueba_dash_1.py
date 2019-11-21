dba248ea24aa00ceff262957e70a45ddf56dbc30
Irad Olivares <irad.olriv@gmail.com>
2019-11-20 23:47:46 -0600

    Update prueba_dash_1.py

--- prueba_dash_1.py
+++ prueba_dash_1.py
@@ -8,6 +8,7 @@ import chart_studio.plotly as py
 import plotly.graph_objs as go
 from numpy import arange,array,ones
 from scipy import stats
+import plotly.io as pio
 
 # Lectura de la base
 bdf = pd.read_spss("C:/Users/IOR_C/Downloads/Base de datos FGF21 final.sav")
@@ -21,15 +22,26 @@ line = slope*bdf["ACTANTIOX_OFICIAL"]+intercept
 # Construcción de la gráfica
 
 fig = go.Figure()
-fig.add_scatter(x=bdf["ACTANTIOX_OFICIAL"], y=bdf["FGF21_OFICIAL"], mode = 'markers')
-fig.add_scatter(x=bdf["ACTANTIOX_OFICIAL"],y=line,mode='lines', marker_color='rgb(31, 119, 180)')
+fig.add_scatter(x=bdf["ACTANTIOX_OFICIAL"], y=bdf["FGF21_OFICIAL"], mode='markers',
+                marker_color='black', hoverinfo="x+y+text")
+fig.add_scatter(x=bdf["ACTANTIOX_OFICIAL"],y=line,mode='lines', marker_color='grey',
+                hoverinfo="x+y+text")
 
 fig.add_annotation(
     go.layout.Annotation(
         x=1000,
         y=3000,
-        text='$R^2 = '+str(round(r_value**2,4))+' p value = '+str(round(p_value,4))+', '+'Y = '+str(round(intercept,4))+' + '+str(round(slope,4))+'X$',
+        text='$R^2 = '+str(round(r_value**2,4))+', p-value = '+str(round(p_value,4))+'$',
         showarrow=False,
-        font_size = 18))
-fig.update_layout(title = 'Linear Fit in Python', showlegend = False)
+        font_size=16,
+        font_color="grey"))
+fig.add_annotation(
+    go.layout.Annotation(
+        x=977,
+        y=2800,
+        text='$Y = ' + str(round(intercept,4))+' + '+str(round(slope,4))+'X$',
+        showarrow=False,
+        font_size=16,
+        font_color="grey"))
+fig.update_layout(title='Linear Fit in Python', showlegend=False, template='plotly_white')
 fig.show()
