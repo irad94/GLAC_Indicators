9da5db28f9b795346e2123f4babb020baf5127c4
Irad Olivares <irad.olriv@gmail.com>
2019-12-07 22:03:35 -0600

    Update app_2v.py

--- app_2v.py
+++ app_2v.py
@@ -12,8 +12,7 @@ import pandas as pd
 import json
 import base64
 import io
-
-# df = pd.read_spss("C:/Users/GLAC/Downloads/Base de datos FGF21 final.sav")
+from dash.exceptions import PreventUpdate
 
 navbar = dbc.NavbarSimple(
     children=[
@@ -45,8 +44,7 @@ body = dbc.Container(
                 html.Div([
                     html.P([html.Strong('Seleccione la variable dependiente', style={'font-size': '120%'})]),
                     dcc.Dropdown(
-                        id='yaxis-column',
-                        value='FGF21_OFICIAL'
+                        id='yaxis-column'
                     )
                 ],
                     style={'width': '50%', 'display': 'inline-block'}),
@@ -54,61 +52,64 @@ body = dbc.Container(
                 html.Div([
                     html.P([html.Strong('Seleccione la variable independiente', style={'font-size': '120%'})]),
                     dcc.Dropdown(
-                        id='xaxis-column',
-                        value='ACTANTIOX_OFICIAL'
+                        id='xaxis-column'
                     )
                 ], style={'width': '50%', 'float': 'right', 'display': 'inline-block'})]),
         dbc.Row([
             html.P("")]),
         dbc.Row([
-            html.Div([
-                dcc.Graph(id='indicator-graphic')])],
-                style={'float': 'right'}),
+            dbc.Col([
+                dbc.FormGroup(
+                    [
+                        dbc.Label("Introduzca el nombre del eje de las ordenadas",
+                                  style={'font-size': '130%', 'margin-top': '15px'}),
+                        dbc.Input(id='input-1-state',
+                                  placeholder="Introduzca el nombre del eje Y",
+                                  type="text", autoComplete='off'),
+                    ]
+                ),
+                dbc.FormGroup(
+                    [
+                        dbc.Label("Introduzca el nombre del eje de las abscisas",
+                                  style={'font-size': '130%'}),
+                        dbc.Input(id='input-2-state',
+                                  placeholder="Introduzca el nombre del eje X",
+                                  type="text",autoComplete='off'),
+                    ]
+                ),
+                html.Hr(style={'width': '100%', 'margin': '0', 'margin-top': '15px', 'margin-bottom': '15px'}),
+                html.Div(
+                    dbc.Button('ACTUALIZAR EJES', id='btn-1', color="primary", className="mr-2",
+                               size="lg", block=True),
+                    style={'width': '100%', 'margin': '0', 'margin-top': '15px', 'margin-bottom': '15px'}),
+                html.Div(
+                    dcc.Upload(
+                        id='upload-data',
+                        children=html.Div([
+                            'Drag and Drop or ',
+                            html.A('Select Files', style={'font-size': '115%'})]),
+                        style={
+                            'width': '100%',
+                            'height': '60px',
+                            'lineHeight': '60px',
+                            'borderWidth': '1px',
+                            'borderStyle': 'dashed',
+                            'borderRadius': '5px',
+                            'textAlign': 'center',
+                        },
+                        # Allow multiple files to be uploaded
+                        multiple=False
+                    ),
+                    style={'width': '100%', 'margin': '0', 'margin-top': '15px', 'margin-bottom': '15px'})
+            ],  md=5),
+            dbc.Col([dcc.Graph(id='indicator-graphic')], md=7),
+            ]),
         dbc.Row([
             html.P("")]),
-        dbc.Row([
-            html.P("Introduzca el nombre del eje de las ordenadas",
-                   style={'font-size': '120%', 'margin-top': '15px', 'margin-bottom': '15px'})]),
-        dbc.Row([
-            dcc.Input(id='input-1-state', type='text', value='FGF21_OFICIAL', size='65')]),
-        dbc.Row([
-            html.P("Introduzca el nombre del eje de las abscisas",
-                   style={'font-size': '120%', 'margin-top': '15px', 'margin-bottom': '15px'})]),
-        dbc.Row([
-            dcc.Input(id='input-2-state', type='text', value='ACTANTIOX_OFICIAL', size='65')]),
-        dbc.Row([
-            html.Hr(style={'width': '97%', 'margin': '0', 'margin-top': '15px', 'margin-bottom': '15px'})]),
-        dbc.Row([
-            dbc.Button('ACTUALIZAR EJES', id='btn-1', color="primary", className="mr-2", size="lg", block=True),
-        ]),
-        dbc.Row([html.Div([
-            dcc.Upload(
-                id='upload-data',
-                children=html.Div([
-                    'Drag and Drop or ',
-                    html.A('Select Files', style={'font-size': '115%'})]),
-                style={
-                    'width': '100%',
-                    'height': '60px',
-                    'lineHeight': '60px',
-                    'borderWidth': '1px',
-                    'borderStyle': 'dashed',
-                    'borderRadius': '5px',
-                    'textAlign': 'center',
-                    'margin-left': '25px',
-                    'margin-right': '140px',
-                    'margin-top': '50px',
-                    'font-size': '115%',
-                },
-                # Allow multiple files to be uploaded
-                multiple=False
-            )
-        ])]),
-        dbc.Row([
-            html.P("")])
     ],
     className="mt-4",
 )
+
 external_stylesheets = [dbc.themes.LUMEN]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -122,31 +123,35 @@ app.layout = html.Div([navbar, body])
     [Input("upload-data", 'contents'),
      Input('upload-data', 'filename')])
 def update_graph(contents, filename):
-    content_type, content_string = contents.split(',')
+    if contents is None:
+        raise PreventUpdate
+    else:
+        content_type, content_string = contents.split(',')
 
-    decoded = base64.b64decode(content_string)
-    try:
-        if 'csv' in filename:
-            # Assume that the user uploaded a CSV file
-            df = pd.read_csv(
-                io.StringIO(decoded.decode('utf-8')))
-            df = df.select_dtypes(include=np.number).dropna(axis=1, how='all')
-            available_indicators = df.columns
-            options1 = [{'label': i, 'value': i} for i in available_indicators]
-            options2 = [{'label': i, 'value': i} for i in available_indicators]
-        elif 'sav' in filename:
-            # Assume that the user uploaded an excel file
-            df = open(decoded)
-            df = df.select_dtypes(include=np.number).dropna(axis=1, how='all')
-            available_indicators = df.columns
-            options1 = [{'label': i, 'value': i} for i in available_indicators]
-            options2 = [{'label': i, 'value': i} for i in available_indicators]
-    except Exception as e:
-        print(e)
-        return html.Div([
-            'There was an error processing this file.'
-        ])
-    return options1, options2
+        decoded = base64.b64decode(content_string)
+        try:
+            if 'csv' in filename:
+                # Assume that the user uploaded a CSV file
+                df = pd.read_csv(
+                    io.StringIO(decoded.decode('utf-8')))
+                df = df.select_dtypes(include=np.number).dropna(axis=1, how='all')
+                available_indicators = df.columns
+                options1 = [{'label': i, 'value': i} for i in available_indicators]
+                options2 = [{'label': i, 'value': i} for i in available_indicators]
+            elif 'xls' in filename:
+                # Assume that the user uploaded an excel file
+                df = pd.read_excel(io.BytesIO(decoded))
+                df = df.select_dtypes(include=np.number).dropna(axis=1, how='all')
+                available_indicators = df.columns
+                options1 = [{'label': i, 'value': i} for i in available_indicators]
+                options2 = [{'label': i, 'value': i} for i in available_indicators]
+        except Exception as e:
+            print(e)
+            return html.Div([
+                'There was an error processing this file.'
+            ])
+        return options1, options2
+
 
 @app.callback(
     Output('indicator-graphic', 'figure'),
@@ -158,84 +163,95 @@ def update_graph(contents, filename):
     [State('input-1-state', 'value'),
      State('input-2-state', 'value')])
 def update_graph(n_clicks1, contents, filename, xaxis_column_name, yaxis_column_name, input_1, input_2):
-    content_type, content_string = contents.split(',')
+    if contents is None:
+        raise PreventUpdate
+    elif xaxis_column_name is None and yaxis_column_name is None:
+        raise PreventUpdate
+    elif xaxis_column_name is None:
+        raise PreventUpdate
+    elif yaxis_column_name is None:
+        raise PreventUpdate
+    else:
+        content_type, content_string = contents.split(',')
 
-    decoded = base64.b64decode(content_string)
-    try:
-        if 'csv' in filename:
-            # Assume that the user uploaded a CSV file
-            df = pd.read_csv(
-                io.StringIO(decoded.decode('utf-8')))
-        elif 'sav' in filename:
-            # Assume that the user uploaded an excel file
-            df = open(decoded)
-    except Exception as e:
-        print(e)
-        return html.Div([
-            'There was an error processing this file.'
-        ])
+        decoded = base64.b64decode(content_string)
+        try:
+            if 'csv' in filename:
+                # Assume that the user uploaded a CSV file
+                df = pd.read_csv(
+                    io.StringIO(decoded.decode('utf-8')))
+            elif 'xls' in filename:
+                # Assume that the user uploaded an excel file
+                df = pd.read_excel(io.BytesIO(decoded))
+        except Exception as e:
+            print(e)
+            return html.Div([
+                'There was an error processing this file.'
+            ])
 
-    df = df.select_dtypes(include=np.number) \
-        .dropna(axis=1, how='all')
+        df = df.select_dtypes(include=np.number) \
+            .dropna(axis=1, how='all')
 
-    dff = df.dropna(subset=[xaxis_column_name, yaxis_column_name])
-    # Construcción del modelo
-    slope, intercept, r_value, p_value, std_err = stats.linregress(dff[xaxis_column_name], dff[yaxis_column_name])
-    line = slope * dff[xaxis_column_name] + intercept
-    return {
-        'data': [dict(
-            x=dff[xaxis_column_name],
-            y=dff[yaxis_column_name],
-            mode='markers',
-            marker={
-                'size': 7,
-                'opacity': 0.6,
-                'color': 'black'
-            },
-            hoverinfo='x+y'
-        ), dict(
-            x=dff[xaxis_column_name],
-            y=line, mode='lines',
-            marker={
-                'size': 9,
-                'opacity': 0.8,
-                'color': 'grey'},
-            hoverinfo="x+y+text")],
-        'layout': dict(
-            xaxis={
-                'title': input_2
-            },
-            yaxis={
-                'title': input_1
-            },
-            margin={'l': 60, 'b': 40, 't': 10, 'r': 10},
-            hovermode='closest',
-            annotations=[dict(
-                x=dff[xaxis_column_name].max()-(dff[xaxis_column_name].max()*0.1),
-                y=dff[yaxis_column_name].max()-(dff[yaxis_column_name].max()*0.1),
-                text='<b>R^2 = '+str(round(r_value**2, 4))+', p-value = '+str(round(p_value, 4))+'<b>',
-                showarrow=False,
-                font_size=10,
-                font_color="grey"
+        dff = df.dropna(subset=[xaxis_column_name, yaxis_column_name])
+        # Construcción del modelo
+        slope, intercept, r_value, p_value, std_err = stats.linregress(dff[xaxis_column_name], dff[yaxis_column_name])
+        line = slope * dff[xaxis_column_name] + intercept
+        return {
+            'data': [dict(
+                x=dff[xaxis_column_name],
+                y=dff[yaxis_column_name],
+                mode='markers',
+                marker={
+                    'size': 7,
+                    'opacity': 0.6,
+                    'color': 'black'
+                },
+                hoverinfo='x+y'
             ), dict(
-                x=dff[xaxis_column_name].max()-(dff[xaxis_column_name].max()*0.12),
-                y=dff[yaxis_column_name].max()-(dff[yaxis_column_name].max()*0.15),
-                text='<b>Y = ' + str(round(intercept, 4)) + ' + ' + str(round(slope, 4)) + 'X<b>',
-                showarrow=False,
-                font_size=16,
-                font_color="grey"
-            )],
-            showlegend=False
-        )
-    }
+                x=dff[xaxis_column_name],
+                y=line, mode='lines',
+                marker={
+                    'size': 9,
+                    'opacity': 0.8,
+                    'color': 'grey'},
+                hoverinfo="x+y+text")],
+            'layout': dict(
+                xaxis={
+                    'title': input_2
+                },
+                yaxis={
+                    'title': input_1
+                },
+                margin={'l': 60, 'b': 40, 't': 10, 'r': 10},
+                hovermode='closest',
+                annotations=[
+                    dict(
+                        x=dff[xaxis_column_name].max()-(dff[xaxis_column_name].max()*0.1),
+                        y=dff[yaxis_column_name].max()-(dff[yaxis_column_name].max()*0.1),
+                        text='<b>R^2 = '+str(round(r_value**2, 4))+', p-value = '+str(round(p_value, 4))+'<b>',
+                        showarrow=False),
+                    dict(
+                        x=dff[xaxis_column_name].max()-(dff[xaxis_column_name].max()*0.12),
+                        y=dff[yaxis_column_name].max()-(dff[yaxis_column_name].max()*0.15),
+                        text='<b>Y = ' + str(round(intercept, 4)) + ' + ' + str(round(slope, 4)) + 'X<b>',
+                        showarrow=False)
+                ],
+                showlegend=False
+            )
+        }
 
 
 @app.callback(
     Output("base-title", 'children'),
     [Input("upload-data", 'filename')])
 def update_graph(filename):
-    return filename
-
+    if filename is None:
+        raise PreventUpdate
+    else:
+        if 'csv' in filename:
+            return str(filename).replace(".csv",'')
+        elif 'xls' in filename:
+            return str(filename).replace(".xls|.xlsx", '')
 
 
 if __name__ == '__main__':
