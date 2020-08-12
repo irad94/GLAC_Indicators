bb9a1e8cfd184996f36b8db385c638a6f5f50447
Irad Olivares <irad.olriv@gmail.com>
2020-08-12 12:37:42 -0500

    Creación de proyecto ML

--- Machine Learning/churn_customer.py
+++ Machine Learning/churn_customer.py
@@ -1,3 +1,4 @@
+# region Ejemplo con sklearn
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -14,6 +15,7 @@ data = pd.read_csv('C:/Users/IOR_C/Downloads/Telco-Customer-Churn.csv')
 sizes = data['Churn'].value_counts(sort = True)
 colors = ["grey","purple"]
 rcParams['figure.figsize'] = 5,5
+
 # Plot
 plt.pie(sizes, colors=colors,
         autopct='%1.1f%%', shadow=True, startangle=270,)
@@ -46,10 +48,17 @@ result = model.fit(X_train, Y_train)
 
 # Test the model
 from sklearn import metrics
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import roc_auc_score
 prediction_test = model.predict(X_test)
 
-# Print the prediction accuracy
+# Print the evaluation
 print(metrics.accuracy_score(Y_test, prediction_test))
+print(classification_report(Y_test, prediction_test))
+print(confusion_matrix(Y_test, prediction_test))
+print(roc_auc_score(Y_test, prediction_test))
+
 
 # To get the weights of all the variables
 weights = pd.Series(model.coef_[0], index=X.columns.values)
@@ -58,7 +67,42 @@ print(weights.sort_values(ascending = False))
 # Pruebas
 dfp = data.copy()
 dfp['pred'] = model.predict(X)
-dfp['pred_prob'] = model.predict_proba(X)[:, 1]
+dfp['pred_prob'] = model.predict_proba(X_test)[:, 1]
 dfp['test'] = np.where(dfp.pred == dfp.Churn, 1, 0)
 
 X_nuevo = np.array([1, 2, 3, 4, 5, 6]).reshape(-1,1)
+# endregion
+
+# region Ejemplo con statsmodels
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+
+# Loading the CSV with pandas
+data = pd.read_csv('C:/Users/IOR_C/Downloads/Telco-Customer-Churn.csv')
+
+# Cleaning dataset
+data.drop(['customerID'], axis=1, inplace=True)
+data['TotalCharges'] = pd.to_numeric(data['TotalCharges'].replace(' ', np.nan))
+data.dropna(subset=['TotalCharges'], inplace=True)
+
+# Prepare the dataset
+data = pd.get_dummies(data, columns=['gender', 'Contract', 'PaymentMethod', 'InternetService'])
+data.replace('No internet service|No phone service', 0, regex=True, inplace=True)
+data.replace('No', 0, regex=True, inplace=True)
+data.replace('Yes', 1, regex=True, inplace=True)
+
+Y = data["Churn"].values
+X = data.drop(labels=["Churn"], axis=1)
+
+# Modelo
+model = sm.Logit(Y, X)
+result = model.fit(method='newton')
+
+logmodel = sm.Logit(Y, sm.add_constant(X))
+logmodel.fit().summary()
+
+result.summary()
+
+
+# endregion
